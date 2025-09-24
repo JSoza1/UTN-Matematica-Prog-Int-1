2ce1717dbad56fa6bed4349d1fcb36f2383060a9
Facundo Tobias Rey <103077968+reyfacundo@users.noreply.github.com>
2025-09-24 16:55:13 -0300

    binario-decimal

--- binario-decimal.py
+++ binario-decimal.py
@@ -1,1 +1,23 @@
-#test
+invalid = True
+
+while invalid:
+    numero = input("Ingrese un número binario: ")
+    digitos = list(numero)
+    for i in range(len(digitos)):
+        if digitos[i] != '1':
+            if digitos[i] != '0':
+                print("El número ingresado no es un binario válido.")
+                break
+    else:
+        invalid = False
+
+digitos.reverse()
+decimal = 0
+print(digitos)
+
+for i in range(len(digitos)):
+    decimal += int(digitos[i]) * 2**i
+
+print(decimal)
+
+# volver al menú, vincular con lógica de hernan
