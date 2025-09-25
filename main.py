82c7f2a96d570a6804c00fdcbc301c5b59e9167d
Hernan Moises <hernanmartinmoises@gmail.com>
2025-09-24 23:00:43 -0300

    Modificado main.py *Se creó el menu principal *Se planteó la lógica para "Decimal a Binario", ubicada en la opcion 1. *Se movió de lugar la lógica planteada para "Binario a Decimal" para que esté acorde a la opcion 2 *Se añadieron/modificaron comentarios para referencias dentro del código.
    Signed-off-by: Hernan Moises <hernanmartinmoises@gmail.com>

--- main.py
+++ main.py
@@ -1,33 +1,63 @@
 
 ###################################################
+#MENU
+while True:
+    print("\n=== Bienvenido al Conversor de números ===")
+    print("1. Decimal a Binario")
+    print("2. Binario a Decimal")
+    print("0. Salir")
 
+    opt = input("Elija una opcion: ")
+    if opt == "1":
+        numero = int(input("Ingrese un numero decimal: "))
 
-############### Binario a Decimal #################
+        if numero == 0:
+            print("El numero binario es 0")
+        else:
+            binario = ""
+            while numero > 0:
+                resto = numero % 2 #calculo del resto
+                binario = str(resto) + binario #colocamos cada nuevo resto al inicio de la cadena
+                numero = numero // 2
+            print("El numero en binario es: ", binario)
 
-invalid = True  # Variable de control(flag) para repetir el ingreso hasta que sea válido
+            #Retorna al menu despues de dar el resultado
 
-# Bucle para validar que el número ingresado sea binario
-while invalid:
-    numero = input("Ingrese un número binario: ")
-    digitos = list(numero)  # Convierte el número ingresado en una lista de caracteres
-    for i in range(len(digitos)):
-        # Verifica que cada dígito sea '0' o '1'
-        if digitos[i] != '1':
-            if digitos[i] != '0':
-                print("El número ingresado no es un binario válido.")
-                break
+    elif opt == "2":
+        ############### Binario a Decimal #################
+
+        invalid = True  # Variable de control(flag) para repetir el ingreso hasta que sea válido
+
+        # Bucle para validar que el número ingresado sea binario
+        while invalid:
+            numero = input("Ingrese un número binario: ")
+            digitos = list(numero)  # Convierte el número ingresado en una lista de caracteres
+            for i in range(len(digitos)):
+                # Verifica que cada dígito sea '0' o '1'
+                if digitos[i] != '1':
+                    if digitos[i] != '0':
+                        print("El número ingresado no es un binario válido.")
+                        break
+            else:
+                # Si todos los dígitos son válidos, sale del bucle
+                invalid = False
+
+        digitos.reverse()  # Invierte la lista para que la posición 0 sea el dígito menos significativo
+        decimal = 0
+        print(digitos)  # Muestra la lista invertida (opcional, para depuración)
+
+        # Recorre la lista y suma el valor decimal correspondiente a cada dígito
+        for i in range(len(digitos)):
+            decimal += int(digitos[i]) * 2 ** i
+
+        print(decimal)  # Muestra el resultado en decimal
+
+        #Retorna al menu despues de dar el resultado
+
+    elif opt == "0":
+        print("Ejecución finalizada")
+        break
     else:
-        # Si todos los dígitos son válidos, sale del bucle
-        invalid = False
+        print("Opcion inválida. Intente de nuevo")
+###################################################
 
-digitos.reverse()  # Invierte la lista para que la posición 0 sea el dígito menos significativo
-decimal = 0
-print(digitos)  # Muestra la lista invertida (opcional, para depuración)
-
-# Recorre la lista y suma el valor decimal correspondiente a cada dígito
-for i in range(len(digitos)):
-    decimal += int(digitos[i]) * 2**i
-
-print(decimal)  # Muestra el resultado en decimal
-
-# volver al menú, vincular con lógica de hernan
